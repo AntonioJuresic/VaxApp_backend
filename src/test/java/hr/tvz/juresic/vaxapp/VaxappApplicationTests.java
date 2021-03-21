f8a971f7255187b1d2a15defd044cb0b3e2c87b5
AntonioJuresic <antonio.juresic.email@gmail.com>
2021-03-21 20:30:02 +0100

    Initial commit

---
+++ src/test/java/hr/tvz/juresic/vaxapp/VaxappApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tvz.juresic.vaxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaxappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
